be18cf54e197b498bbb6adbcc6c65403eb1a8424
”Frederik <fred295q@stud.kea.dk>
2023-12-11 09:31:19 +0100

    console out

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -10,9 +10,7 @@ export default function Program({ bands, schedule }) {
     // Variable for handeling newBands - a combination of both the /bands endpoint and the /schedule endpoint
     for (const stage in schedule) {
       for (const day in schedule[stage]) {
-        const found = schedule[stage][day].find(
-          (item) => item.act == band.name
-        );
+        const found = schedule[stage][day].find((item) => item.act == band.name);
         if (found) {
           return {
             ...band,
@@ -26,14 +24,18 @@ export default function Program({ bands, schedule }) {
     }
     return band; // I dont get 60% of this code, credit is Jonas' it works really well.
   });
-  console.log(newBands);
-  console.log(schedule);
+  //console.log(newBands);
+  //console.log(schedule);
 
   const uniqueGenres = [...new Set(bands.map((band) => band.genre))]; // Completely stole this code, it's hella smart. Set is creating a new array with no duplicate genres.
   const [genre, setGenre] = useState("All"); // this is the state that will determine what genre we will display
 
+  let storedFavorites = [];
   const lsKey = "favoriteBands";
-  const storedFavorites = JSON.parse(localStorage.getItem(lsKey)) || [];
+  if (typeof window !== "undefined") {
+    storedFavorites = JSON.parse(localStorage.getItem(lsKey)) || [];
+  }
+
   const [favorite, setFavorite] = useState(storedFavorites);
   const [showFaves, setShowFaves] = useState(false);
 
@@ -45,9 +47,7 @@ export default function Program({ bands, schedule }) {
 
   const toggleFavorite = (bandName) => {
     setFavorite((prevFavorites) => {
-      const updatedFavorites = prevFavorites.includes(bandName)
-        ? prevFavorites.filter((name) => name !== bandName)
-        : [...prevFavorites, bandName];
+      const updatedFavorites = prevFavorites.includes(bandName) ? prevFavorites.filter((name) => name !== bandName) : [...prevFavorites, bandName];
 
       // Save the updated favorites to local storage
       localStorage.setItem(lsKey, JSON.stringify(updatedFavorites));
@@ -58,13 +58,7 @@ export default function Program({ bands, schedule }) {
   console.log(favorite);
 
   // OMEGA FILTERING
-  const filteredBands = newBands.filter(
-    (band) =>
-      (!showFaves || favorite.includes(band.name)) &&
-      (genre === "All" || band.genre === genre) &&
-      (dayFilter === "All" || band.day === dayFilter) &&
-      band.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredBands = newBands.filter((band) => (!showFaves || favorite.includes(band.name)) && (genre === "All" || band.genre === genre) && (dayFilter === "All" || band.day === dayFilter) && band.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
   const bandComps = filteredBands.map(
     (
@@ -83,11 +77,7 @@ export default function Program({ bands, schedule }) {
     <div className="h-screen">
       <div>
         <label htmlFor="genreFilter">Filter by Genre:</label>
-        <select
-          id="genreFilter"
-          onChange={(e) => setGenre(e.target.value)}
-          value={genre}
-        >
+        <select id="genreFilter" onChange={(e) => setGenre(e.target.value)} value={genre}>
           <option value="All">All Genres</option>
           {uniqueGenres.map((uniqueGenre) => (
             <option value={uniqueGenre} key={uniqueGenre}>
@@ -98,11 +88,7 @@ export default function Program({ bands, schedule }) {
       </div>
       <div>
         <label htmlFor="dayFilter">Filter by Day:</label>
-        <select
-          id="dayFilter"
-          onChange={(e) => setDayFilter(e.target.value)}
-          value={dayFilter}
-        >
+        <select id="dayFilter" onChange={(e) => setDayFilter(e.target.value)} value={dayFilter}>
           <option value="All">All Days</option>
           {uniqueDaysHard.map((uniqueDay) => (
             <option value={uniqueDay} key={uniqueDay}>
@@ -113,22 +99,13 @@ export default function Program({ bands, schedule }) {
       </div>
       <div>
         <label>
-          <input
-            type="checkbox"
-            checked={showFaves}
-            onChange={() => setShowFaves(!showFaves)}
-          />
+          <input type="checkbox" checked={showFaves} onChange={() => setShowFaves(!showFaves)} />
           Show Favorites
         </label>
       </div>
       <div>
         {/* ... (existing code) */}
-        <input
-          type="text"
-          placeholder="Search bands..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        <input type="text" placeholder="Search bands..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
       </div>
       <div className="flex flex-col gap-4">
         {filteredBands.length === 0 ? ( // If the filteredBands array is empty, then display a small message for the user
