798f728e3257f56a4474b932bb2fc3af8408c562
”Frederik <fred295q@stud.kea.dk>
2023-12-11 09:07:46 +0100

    eslint

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 export default function Map() {
   return (
     <div>
-      <p>HER SKAL DU "MAP MAP MAP" NOGLE TING</p>
+      <p>HER SKAL DU &quot;MAP MAP MAP&quot; NOGLE TING</p>
     </div>
   );
 }
