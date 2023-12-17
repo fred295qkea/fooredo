f51fba116d28021e69d7aabab67a46998fc473cf
”Frederik <fred295q@stud.kea.dk>
2023-12-17 19:54:10 +0100

    hovsa

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Hero from "./Hero";
 import Cancel from "./Cancel";
+import Live from "./Live";
 
 export default function Frontpage() {
   return (
     <div className=" bg-mainBG">
       <Hero />
       <Cancel />
+      <Live />
     </div>
   );
 }
