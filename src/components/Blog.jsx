2cad8bf8bea63ac0a008d2ff82b3b4395c8539b6
”Frederik <fred295q@stud.kea.dk>
2024-01-16 22:44:14 +0100

    float on live and blog border-r

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -36,23 +36,24 @@ export default async function Blog() {
 
   const blogs = data.allBlogs;
 
-  console.log("blogs:", blogs);
-
   return (
-    <section className="grid grid-cols-2 m-auto lg:grid-cols-3 max-w-7xl">
-      {blogs.map((blog) => {
-        return (
-          <article className="flex flex-col gap-4 m-3 border-2 rounded-md first:col-span-2 first:row-span-2" key={blog.id}>
-            <Link href={`/blogs/${blog.id}`}>
-              <Image className="w-full rounded-md " src={blog.blogImage.url} alt="Blog image" width={400} height={400} sizes="50vw" />
-              <div className="flex flex-col p-3">
-                <h3 className="text-white first:text-xl ">{blog.headline}</h3>
-                <p className="text-sm text-white first:text-lg">{blog.description} </p>
-              </div>
-            </Link>
-          </article>
-        );
-      })}
-    </section>
+    <div className="mt-5">
+      <h3 className="px-4 m-auto text-2xl text-white max-w-7xl">News</h3>
+      <section className="grid grid-cols-2 m-auto lg:grid-cols-3 max-w-7xl">
+        {blogs.map((blog) => {
+          return (
+            <article className="flex flex-col gap-4 m-3 border-2 first:col-span-2 first:row-span-2" key={blog.id}>
+              <Link href={`/blogs/${blog.id}`}>
+                <Image className="w-full rounded-md " src={blog.blogImage.url} alt="Blog image" width={400} height={400} sizes="50vw" />
+                <div className="flex flex-col p-3">
+                  <h3 className="text-white first:text-2xl ">{blog.headline}</h3>
+                  <p className="text-sm text-white first:text-lg">{blog.description} </p>
+                </div>
+              </Link>
+            </article>
+          );
+        })}
+      </section>
+    </div>
   );
 }
--- src/components/Livecard.jsx
+++ src/components/Livecard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Livecard({ liveact, img, text }) {
   return (
-    <article className="text-white border w-80">
+    <article className="my-8 text-white border w-80 first:translate-y-6 last:translate-y-3">
       <Image src={img} width={300} height={300} sizes="50vw" alt="Live image from concert" className="object-cover w-full aspect-video" />
       <div className="flex flex-col gap-2 p-4">
         <h3 className="text-xl uppercase">{liveact.act} - Live</h3>
