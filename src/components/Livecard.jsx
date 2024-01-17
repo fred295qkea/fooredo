2b0b3b01288d9e56697dc7903d395d41e606c6e4
”Frederik <fred295q@stud.kea.dk>
2024-01-17 10:52:16 +0100

    korrektur

--- src/components/Livecard.jsx
+++ src/components/Livecard.jsx
@@ -6,7 +6,7 @@ function Livecard({ liveact, img, text }) {
       <Image src={img} width={300} height={300} sizes="50vw" alt="Live image from concert" className="object-cover w-full aspect-video" />
       <div className="flex flex-col gap-2 p-4">
         <h3 className="text-xl uppercase">{liveact.act} - Live</h3>
-        <p className="self-end">
+        <p className="self-end first-letter:uppercase">
           {liveact.act + " "}
           {text}
         </p>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,7 @@ export default function Map() {
   return (
     <section className="pt-20 text-white">
       <h1 className="text-3xl text-center my-7">Map</h1>
-      <p className="mx-10 text-center">Having trouble finding finding your way around? download the map!</p>
+      <p className="mx-10 text-center">Having trouble finding your way around? download the map!</p>
       <div className="p-5 m-auto my-2 border-2 border-solid rounded-xl border-accent max-w-7xl">
         <Image src={"/map.png"} height={900} width={1600} sizes="100wv" alt="Map of festival"></Image>
       </div>
